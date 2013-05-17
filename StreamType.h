5e8ec6115bc4a829aa5aa59f8c2a13083cd3044f
isxxguo <isxxguo@pptv.com>
2013-05-17 08:50:45 +0000

    /ppbox/branches/1.2.0 /src/ppbox 修正一些文件不存在的 include emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@6774 b3a1389e-2159-5c43-9038-463bf877d0d9

--- StreamType.h
+++ StreamType.h
@@ -3,26 +3,12 @@
 #ifndef _PPBOX_AVBASE_FORMAT_H_
 #define _PPBOX_AVBASE_FORMAT_H_
 
-#include <boost/detail/endian.hpp>
+#include "ppbox/avbase/FourCC.h"
 
 namespace ppbox
 {
     namespace avbase
     {
-
-#ifdef BOOST_BIG_ENDIAN
-#define MAKE_FOURC_TYPE(c1, c2, c3, c4) \
-    ((((boost::uint32_t)c1) << 24) | \
-    (((boost::uint32_t)c2) << 16) | \
-    (((boost::uint32_t)c3) << 8) | \
-    (((boost::uint32_t)c4)))
-#else
-#define MAKE_FOURC_TYPE(c1, c2, c3, c4) \
-    ((((boost::uint32_t)c1)) | \
-    (((boost::uint32_t)c2) << 8) | \
-    (((boost::uint32_t)c3)) << 16 | \
-    (((boost::uint32_t)c4) << 24))
-#endif
 
         struct StreamType
         {
