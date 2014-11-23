92a0686f96d91af17d37ee797a85f565727060a1
isxxguo <isxxguo@pptv.com>
2014-11-23 11:59:14 +0800

    增加字幕 SUBS 流类型
    Change-Id: Ieb3cda191fb9e0841a29da0ad7fee6207bc7f7bf

--- StreamType.h
+++ StreamType.h
@@ -17,6 +17,7 @@ namespace ppbox
                 NONE = 0, 
                 VIDE = MAKE_FOURC_TYPE('V', 'I', 'D', 'E'), 
                 AUDI = MAKE_FOURC_TYPE('A', 'U', 'D', 'I'), 
+                SUBS = MAKE_FOURC_TYPE('S', 'U', 'B', 'S'), 
             };
         };
 
